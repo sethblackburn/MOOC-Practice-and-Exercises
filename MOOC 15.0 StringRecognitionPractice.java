f31784f8434863b0ac06975bc9aaef95d80547eb
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-11 17:54:29 -0500

    Update MOOC 15.0 StringRecognitionPractice.java

--- MOOC 15.0 StringRecognitionPractice.java
+++ MOOC 15.0 StringRecognitionPractice.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class StringRecognitionPractice {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
