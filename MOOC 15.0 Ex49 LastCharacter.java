f146abc8a4694ff75448591ffdaa770943104b66
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-28 16:27:47 -0500

    Update MOOC 15.0 Ex49 LastCharacter.java

--- MOOC 15.0 Ex49 LastCharacter.java
+++ MOOC 15.0 Ex49 LastCharacter.java
@@ -1,4 +1,4 @@
-// MOOC 15.0 Ex49; https://materiaalit.github.io/2013-oo-programming/part1/week-3/
+// MOOC 15.0 Ex49; https://materiaalit.github.io/2013-oo-programming/part1/week-3/; 28 Oct 2019
 
 import java.util.Scanner;
 
