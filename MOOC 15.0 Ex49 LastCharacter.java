1a008af2bbf90cfea56051861c02ea8f79292c55
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-28 16:13:52 -0500

    Create MOOC 15.0 Ex49 LastCharacter.java

---
+++ MOOC 15.0 Ex49 LastCharacter.java
@@ -0,0 +1,17 @@
+// MOOC 15.0 Ex49; https://materiaalit.github.io/2013-oo-programming/part1/week-3/
+
+import java.util.Scanner;
+
+public class LastCharacter {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String name = reader.next();
+
+        System.out.print("Last Letter: " + calculateCharacters(name));
+    }
+    public static char calculateCharacters(String text) {
+        return text.charAt(text.length()-1);
+    }
+}
