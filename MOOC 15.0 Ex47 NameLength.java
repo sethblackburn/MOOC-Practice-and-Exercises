83d381eb0371554d04f105f3e608d8154bfd4dce
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-28 14:06:32 -0500

    Create MOOC 15.0 Ex47 NameLength.java

---
+++ MOOC 15.0 Ex47 NameLength.java
@@ -0,0 +1,17 @@
+//MOOC 15.0 Ex47; https://materiaalit.github.io/2013-oo-programming/part1/week-3/; 28 Oct 2019
+
+import java.util.Scanner;
+
+public class nameLength {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String name = reader.next();
+
+        System.out.print("Number of characters: " + calculateCharacters(name));
+    }
+    public static int calculateCharacters(String text) {
+        return text.length();
+    }
+}
