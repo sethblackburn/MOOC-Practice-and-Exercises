29da8ebedf74d1a297edf37cd7a0e1bb357dee33
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-29 13:59:29 -0500

    Create MOOC 15.1 Ex55 FirstIndex.java

---
+++ MOOC 15.1 Ex55 FirstIndex.java
@@ -0,0 +1,23 @@
+// MOOC 15.1 Ex55; https://materiaalit.github.io/2013-oo-programming/part1/week-3/; 29 Oct 2019
+
+import java.util.Scanner;
+
+public class FirstIndex {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type the first word: ");
+        String firstWord = reader.nextLine();
+
+        System.out.print("Type the second word: ");
+        String secondWord = reader.nextLine();
+
+        int index = firstWord.indexOf(secondWord);
+
+        if (index >= 0 && index <= firstWord.length()) {
+            System.out.print("The word '" + secondWord + "' is found in the word '" + firstWord + "'.");
+        } else if (index < 0) {
+            System.out.print("The word '" + secondWord + "' is not found in the word '" + firstWord + "'.");
+        }
+    }
+}
