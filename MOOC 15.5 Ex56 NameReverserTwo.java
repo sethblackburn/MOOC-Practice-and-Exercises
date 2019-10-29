fb1e38737e3d64c3d519d19aa28401850d8ac75e
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-29 14:57:56 -0500

    Create MOOC 15.5 Ex56 NameReverserTwo.java

---
+++ MOOC 15.5 Ex56 NameReverserTwo.java
@@ -0,0 +1,22 @@
+// MOOC 15.5 Ex56; https://materiaalit.github.io/2013-oo-programming/part1/week-3/; 29 Oct 2019
+// Mirrors Ex52, but uses a seperate method
+
+import java.util.Scanner;
+
+public class NameReverserTwo {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type a text: ");
+        String text = reader.nextLine();
+
+        reverser(text);
+    }
+    public static void reverser(String text) {
+        int counter = 1;
+        while (counter <= text.length()) {
+            System.out.print(text.charAt(text.length() - counter));
+            counter++;
+        }
+    }
+}
