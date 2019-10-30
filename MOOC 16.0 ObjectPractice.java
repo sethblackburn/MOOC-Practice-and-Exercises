0ffabc50f08adc9346379d9bfe123aac25fc1897
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-30 14:54:18 -0500

    Create MOOC 16.0 ObjectPractice.java

---
+++ MOOC 16.0 ObjectPractice.java
@@ -0,0 +1,21 @@
+// MOOC 16.0 ObjectPractice; 30 Oct 2019
+
+public class ObjectPractice {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+    // Strings are objects, meaning they have a value and they have methods.
+    // Objects, like the Scanner above ^^^ must be created with the "new" command.
+    // Strings are exceptional, they can be created with the "new" command, as shown:
+
+        String banana = new String("Banana");
+
+    // Strings can also be created in the same way as a variable/value, as I've done so far:
+
+        String carrot = "Carrot";
+
+        System.out.println(banana);
+
+        System.out.println(carrot);    
+    }
+}
