1d32df5f990664b078387b6f5198f6aa6a1c7ad2
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-10-30 16:11:59 -0500

    Update and rename MOOC 15.1 SubstringPractice.java to MOOC 15.1 ASubstringPractice.java

--- MOOC 15.1 ASubstringPractice.java
+++ MOOC 15.1 ASubstringPractice.java
@@ -1,7 +1,7 @@
 // MOOC 15.1 Substring Practice; https://materiaalit.github.io/2013-oo-programming/part1/week-3/; 28 Oct 2019
 
 public class Main {
-    public static void SubstringPractice (String[] args) {
+    public static void ASubstringPractice (String[] args) {
         String word = "antidisestablishmentarianism";
         
         String end = word.substring(25); //starts at 25, goes to the end, extracts value "ism" for String end
