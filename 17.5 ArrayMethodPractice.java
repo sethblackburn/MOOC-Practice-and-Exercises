f276147057295b1a6d3dddc209378939a9b62d70
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2019-12-11 15:42:00 -0600

    Update 17.5 ArrayMethodPractice.java

--- 17.5 ArrayMethodPractice.java
+++ 17.5 ArrayMethodPractice.java
@@ -1,8 +1,7 @@
-//MOOC 17.5 ArrayMethodPractice; https://materiaalit.github.io/2013-oo-programming/part1/week-3/ 11 Dec 2019
+//MOOC 17.5 ArrayMethodPractice; https://materiaalit.github.io/2013-oo-programming/part1/week-3/; 11 Dec 2019
 
 //Method input and output is my weakness at this point, though it does make more sense as I go on. Just gotta keep going.
 
-import java.util.Collections;
 import java.util.ArrayList;
 
 public class ArrayMethodPractice {
